e049ce658a008f90b20188e3394d583d720cc42f
Pedro Germano <pedrogermano232@gmail.com>
2025-04-23 19:39:24 -0300

    first video done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 import { Canvas } from "./canvas.js";
+import { services } from "./services.js";
 
 const canvasElements = [...document.querySelectorAll('canvas')]
+const aboutTiles = document.querySelector('.about__tiles')
 
 console.log(canvasElements)
 
@@ -9,6 +11,24 @@ let canvasArray = []
 canvasElements.forEach(canvas => {
   let squareCanvas = new Canvas(canvas)
   canvasArray.push(squareCanvas)
+})
+
+services.forEach(service => {
+  let tile = document.createElement('div')
+  tile.classList.add('service__tile')
+
+  let icon = document.createElement('div')
+  icon.innerHTML = service.icon
+  tile.appendChild(icon)
+
+  let serviceTitle = document.createElement('h4')
+  serviceTitle.textContent = service.title
+
+  let serviceDesc = document.createElement('p')
+  serviceDesc.textContent = service.description
+  tile.append(serviceTitle, serviceDesc)
+  aboutTiles.appendChild(tile)
+
 })
 
 window.addEventListener('resize', () => {
